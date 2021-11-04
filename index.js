e5236afd1c5dae2fa0455e5d1c1dda53a4117da9
PaulNguyen <paulnguyen2207@gmail.com>
2021-11-04 15:04:46 +1100

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData={
+    "email":"",
+    "name":""
+}
+
+const configObj={
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+    },
+    body:JSON.stringify(
+       (submitData)
+    )}
+
+
+fetch("http://localhost:3000/users",configObj)
+.then (res=>res.json)
+.then( function ( object ) {
+    document.body.innerHTML = object[ "id" ]
+  } )
+  .catch( function ( error ) {
+    document.body.innerHTML = error.message
+  } )
+
+  
